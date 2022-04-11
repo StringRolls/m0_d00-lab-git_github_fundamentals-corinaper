d759fc3042477552df2bec61b1c980cc26989411
Corina Perjan <cperjan@gmail.com>
2022-04-11 15:17:23 +0200

    delete:alert; add:alert

--- exercise.js
+++ exercise.js
@@ -1,4 +1,6 @@
 // This file was created live
 
 console.log("Hello Ironhackers!", "This is the first demo of Live Share", "Tell me how it's going")
-alert("Hello world")
+alert("Hello world");
+console.log("something");
+
